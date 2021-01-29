8d7cf0efab607b859916240baa0e7282a2509cea
jordan-tuinman <jojotuijo@gmail.com>
2021-01-29 13:22:42 +1300

    commit of partial work

--- client/components/Tile.jsx
+++ client/components/Tile.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Tile (props) {
   const isVisible = props.isVisible
   if (isVisible) {
-    return <img src={props.src} className='tile'></img>
-  } else return <div className='tile'></div>
+    return <img onClick={props.onClick} src={props.src} className='tile'></img>
+  } else return <div onClick={props.onClick} className='tile'></div>
 }
 
 export default Tile

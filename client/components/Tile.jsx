675e27ca230a5e37e19b3820d1d5f30f495d65d7
jordan-tuinman <jojotuijo@gmail.com>
2021-01-29 14:33:05 +1300

    make tiles visible with function

--- client/components/Tile.jsx
+++ client/components/Tile.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Tile (props) {
   const isVisible = props.isVisible
-  if (isVisible) {
-    return <img onClick={props.onClick} src={props.src} className='tile'></img>
-  } else return <div onClick={props.onClick} className='tile'></div>
+  const [visibility, setVisibility] = useState(isVisible)
+
+  function setTileVisible () {
+    setVisibility(true)
+  }
+  return (
+    visibility
+      ? <img src={props.src} className='tile' />
+      : <div onClick={setTileVisible} className='tile'></div>
+  )
 }
 
 export default Tile
